7159cf66f9bcf37d0d35997392b6a8499ea4d08f
Elena Lambova <elena.g.lambova@gmail.com>
2020-05-06 22:54:23 +0300

    Feat: Add functionality for combine adding functionality that unites all components.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as BooksAPI from "./APIConnect/BooksAPI";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Bookshelf from "./Components/Content/MainPage/Bookshelf";
+import SearchBook from "./Components/Content/SearchPage/SearchBook";
+import { Route } from "react-router-dom";
+import OpenSearchWindow from "./Components/Footer/OpenSearchWindow";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class BooksApp extends React.Component {
+  state = {
+    books: [],
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState(() => ({ books }));
+    });
+  }
+
+  updateBookShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).then((response) => {
+      BooksAPI.getAll().then((books) => {
+        this.setState(() => ({ books }));
+      });
+    });
+  };
+
+  render() {
+    const { books } = this.state;
+    return (
+      <div className="app">
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="list-books">
+              <Header />
+              <div className="list-books-content">
+                <div>
+                  <Bookshelf
+                    bookshelfTitle="Currently Reading"
+                    updateBookShelf={(book, shelf) => {
+                      this.updateBookShelf(book, shelf);
+                    }}
+                    bookshelfName="currentlyReading"
+                    books={books}
+                  />
+                  <Bookshelf
+                    bookshelfTitle="Want to Read"
+                    updateBookShelf={(book, shelf) => {
+                      this.updateBookShelf(book, shelf);
+                    }}
+                    bookshelfName="wantToRead"
+                    books={books}
+                  />
+                  <Bookshelf
+                    bookshelfTitle="Read"
+                    updateBookShelf={(book, shelf) => {
+                      this.updateBookShelf(book, shelf);
+                    }}
+                    bookshelfName="read"
+                    books={books}
+                  />
+                </div>
+                <OpenSearchWindow />
+              </div>
+            </div>
+          )}
+        />
+        <Route
+          path="/search"
+          render={() => (
+            <SearchBook books={books} updateBookShelf={this.updateBookShelf} />
+          )}
+        />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default BooksApp;
